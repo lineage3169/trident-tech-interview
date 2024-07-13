928695416e947e99e872ad79751161546da8fd92
Lancelot <rwpbcyw5@gmail.com>
2024-07-13 21:41:33 +0800

    git init

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <main>
+    <div class="list no-select">
+      <ListItem v-model="aldult" label="大人" :add-disabled="addDisabled"></ListItem>
+      <ListItem v-model="child" label="小孩" :add-disabled="addDisabled || aldult === 0"></ListItem>
+      <ListItem v-model="baby" label="嬰兒" :add-disabled="babyAddDisabled"></ListItem>
+    </div>
+  </main>
+</template>
+<script setup>
+import { computed, ref, watch } from "vue";
+import ListItem from "./components/list-item.vue";
+const aldult = ref(0)
+const child = ref(0)
+const baby = ref(0)
+const limit = 9
+
+const addDisabled = computed(() => {
+  return aldult.value + child.value + baby.value >= limit
+})
+
+const babyAddDisabled = computed(() => {
+  return addDisabled.value || aldult === 0 || baby.value >= aldult.value
+})
+
+watch(aldult, (newVal, oldVal) => {
+  if ((newVal < oldVal) && baby.value > 0) {
+    baby.value--
+  } 
+})
+
+</script>
+<style lang="scss" scoped>
+.list {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
---
+++ src/components/list-item.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="list-item">
+    <div class="list-item-label">
+      {{ label }}
+    </div>
+    <button class="button minus-button" :class="{'button--disabled': minusDisabled }" @click="minus">
+      <PhMinusCircle :size="32" :color="minusDisabled ? '#888888' : '#000'" weight="fill" />
+    </button>
+    <div class="count">
+      {{ count }}
+    </div>
+    <button class="button add-button" @click="add" :class="{'button--disabled': addDisabled }">
+      <PhPlusCircle :size="32" :color="addDisabled ? '#888888' : '#000'" weight="fill" />
+    </button>
+  </div>
+</template>
+<script setup>
+import { ref, computed } from 'vue'
+import { PhPlusCircle, PhMinusCircle } from "@phosphor-icons/vue"
+const props = defineProps({
+  label: {
+    type: String,
+    default: ''
+  },
+  addDisabled: {
+    type: Boolean,
+    default: false
+  }
+})
+
+const count = defineModel({ default: 0 })
+
+const add = () => {
+  if (!props.addDisabled)
+    count.value++
+}
+
+const minus = () => {
+  if (count.value > 0)
+    count.value-- 
+}
+
+const minusDisabled = computed(() => count.value === 0)
+
+</script>
+<style lang="scss" scoped>
+.list-item {
+  display: flex;
+  margin-bottom: 6px;
+  justify-content: center;
+  align-items: center;
+  &:last-child {
+    margin-bottom: 0;
+  }
+  &-label {
+    padding: 2px 16px;
+  }
+  .count {
+    font-size: 18px;
+    padding: 0 4px;
+  }
+  .button {
+    background-color: transparent;
+    padding: 0;
+    width: 32px;
+    height: 32px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    box-sizing: border-box;
+    outline: none;
+  }
+}
+</style>
